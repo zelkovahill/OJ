5cd6f1e96cf0820f2be1b98d5a1501a50f04bebe
ChangWooMin <127643365+zelkovahill@users.noreply.github.com>
2023-06-28 20:32:25 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(void)
+{
+    int sum=0;
+int a;
+
+    
+    std::cin>>a;
+
+    for(int i=1;i<=a;i++)
+    {
+sum=sum+i;
+
+    }
+    std::cout<<sum<<std::endl;
+    return 0;
+}
+
