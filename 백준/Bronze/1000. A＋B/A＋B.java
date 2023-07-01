d4f068f81189d64210e1fed286351b75057a0e00
ChangWooMin <127643365+zelkovahill@users.noreply.github.com>
2023-07-01 23:24:18 +0900

    [Bronze V] Title: A+B, Time: 220 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+
+        Scanner c = new Scanner(System.in);
+
+         int a,b;
+
+         a = c.nextInt();
+         b = c.nextInt();
+
+
+        System.out.println(a+b);
+
+
+    }
+}
