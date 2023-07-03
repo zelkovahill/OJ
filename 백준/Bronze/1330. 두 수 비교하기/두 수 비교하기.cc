58d984d9e84d860453879b3b5397a1471672d075
ChangWooMin <127643365+zelkovahill@users.noreply.github.com>
2023-07-03 21:40:20 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int A,B;
+    cin>>A>>B;
+    
+    if(A>B)
+    {
+        cout<<">";
+    }
+    
+    else if(A<B)
+    {
+        cout<<"<";
+    }
+    
+    else
+    {
+        cout<<"==";
+    }
+    
+    return 0;
+}
