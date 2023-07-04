87497ae8a14a23320a22e135ef59673cf662a450
ChangWooMin <127643365+zelkovahill@users.noreply.github.com>
2023-07-04 21:29:59 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    
+    if(a>=90)
+    {
+        printf("A");
+    }
+    
+    else if(80<=a&&a<=89)
+    {
+        printf("B");
+    }
+    
+    else if(70<=a&&a<=79)
+    {
+        printf("C");
+    }
+    
+    else if(60<=a&&a<=69)
+    {
+        printf("D");
+    }
+    
+    else
+    {
+        printf("F");
+    }
+    return 0;
+    
+}
