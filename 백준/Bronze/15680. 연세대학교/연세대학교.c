eb90fc6c586a1d7b97f28bc90606fa0e95fa66f0
ChangWooMin <127643365+zelkovahill@users.noreply.github.com>
2023-07-07 08:59:48 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    
+    scanf("%d",&a);
+    
+    if(a==0)
+    {
+        printf("YONSEI\n");
+    }
+    
+    if(a==1)
+    {
+        printf("Leading the Way to the Future\n");
+    }
+    return 0;
+}
