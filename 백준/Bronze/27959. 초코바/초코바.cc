f4e2c8bc3073afbc713be469401c3f37ae85cd63
ChangWooMin <127643365+zelkovahill@users.noreply.github.com>
2023-07-15 02:00:23 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N,M; // N 100원의 동전 개수 // M원인 초코바
+cin>>N >> M;
+N=N*100;
+
+if(N>=M)
+{
+cout<<"Yes"<<endl;
+}
+
+else
+{
+cout<<"No"<<endl;
+}
+
+
+
+}
