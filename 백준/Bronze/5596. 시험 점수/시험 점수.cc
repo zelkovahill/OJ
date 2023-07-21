dd1b33c33b18300ead49a2cefd3f52076c30cae2
ChangWooMin <127643365+zelkovahill@users.noreply.github.com>
2023-07-21 10:20:53 +0900

    [Bronze IV] Title: 시험 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5596. 시험 점수/시험 점수.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+ 
+  int A,B;
+  int a,b,c,d;
+
+  cin>>a>>b>>c>>d;
+  A=a+b+c+d;
+
+  cin>>a>>b>>c>>d;
+  B=a+b+c+d;
+
+  if(A>B) cout<<A<<endl;
+
+  else cout<<B<<endl;
+
+
+
+
+
+
+}
