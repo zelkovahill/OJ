514fc9df0d6bd89bb8323056cc38deaf4117df90
ChangWooMin <127643365+zelkovahill@users.noreply.github.com>
2023-07-25 00:18:24 +0900

    [Bronze IV] Title: Fill the Rowboats!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5300. Fill the Rowboats！/Fill the Rowboats！.cc
@@ -0,0 +1,43 @@
+#include <iostream> 
+
+
+using namespace std;
+
+
+
+int main()
+{
+
+  int a;
+
+  cin>>a;
+
+  for(int i=1; i<=a;i++)
+  {
+    cout<<i<<" ";
+    if(i%6==0)
+    { 
+      if(i==a)   continue;
+      cout<<"Go! ";  
+    
+    }
+    
+   
+      
+       
+    
+    
+    
+   
+  }
+
+cout<<"Go! "<<endl;
+
+
+  
+
+  
+
+
+
+}
