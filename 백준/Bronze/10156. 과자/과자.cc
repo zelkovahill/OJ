24e02568377ca7bf4c173e51ee7fe280ea524b72
ChangWooMin <127643365+zelkovahill@users.noreply.github.com>
2023-07-25 00:25:46 +0900

    [Bronze IV] Title: 과자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.cc
@@ -0,0 +1,28 @@
+#include <iostream> 
+
+
+using namespace std;
+
+
+
+int main()
+{
+  int K , N , M ;
+
+  cin>>K>>N>>M;
+
+  if(K*N>=M)
+  {
+    cout<<K*N-M<<endl;
+  }
+
+  else
+  {
+    cout<<"0"<<endl;
+  }
+
+  
+
+
+
+}
