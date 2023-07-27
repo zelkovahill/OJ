a3487908a334ff5c5b5c35df5e294657fe8cfee0
ChangWooMin <127643365+zelkovahill@users.noreply.github.com>
2023-07-28 08:34:17 +0900

    [Bronze IV] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+int main()
+{
+    long N , M;
+
+    cin>>N>>M;
+
+    if(N==M==1)
+    {
+        cout<<"1"<<endl;
+    }
+
+
+    else
+    {
+    cout<<"0"<<endl;
+    }
+    
+}
