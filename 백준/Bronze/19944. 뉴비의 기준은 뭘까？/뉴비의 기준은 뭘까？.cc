e6da0a20e17e9a855808e246a9cc300b14397e87
ChangWooMin <127643365+zelkovahill@users.noreply.github.com>
2023-07-28 19:45:10 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N,M;
+
+    cin>>N>>M;
+
+    if(M==1||M==2) 
+    {
+    cout<<"NEWBIE!"<<endl;
+    }
+
+    else if(M>2&&M<=N) cout<<"OLDBIE!"<<endl;
+
+    else cout<<"TLE!"<<endl;
+}
