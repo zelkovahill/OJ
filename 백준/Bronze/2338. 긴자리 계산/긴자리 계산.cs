296d64d1cdaa0adff2e5686b5d9a8d7f604e10a1
ChangWooMin <changwoomin426@gmail.com>
2023-07-30 17:26:19 +0900

    [Bronze V] Title: 긴자리 계산, Time: 64 ms, Memory: 5692 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Numerics;
+
+string A = Console.ReadLine();
+string B = Console.ReadLine();
+
+BigInteger a = BigInteger.Parse(A);
+BigInteger b = BigInteger.Parse(B);
+
+
+Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+Console.WriteLine(a * b);
+
