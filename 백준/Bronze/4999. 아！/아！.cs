50e92d063a358280d12868211f25fe047cbbddce
ChangWooMin <changwoomin426@gmail.com>
2023-08-02 11:02:21 +0900

    [Bronze V] Title: 아!, Time: 64 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cs
@@ -0,0 +1,12 @@
+string a = Console.ReadLine();// 가장 길게 낼 수 있는 목소리 
+string b = Console.ReadLine();// 의사가 듣고 싶은 것 
+
+if(a.Length>=b.Length)
+{
+    Console.WriteLine("go");
+}
+
+else
+{
+    Console.WriteLine("no");
+}
