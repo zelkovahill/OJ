e3e3f85b20ae19a5f278b6b65713e183ddc9e62d
ChangWooMin <changwoomin426@gmail.com>
2023-08-05 14:55:58 +0900

    [Bronze V] Title: 時間 (Hour), Time: 208 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.java
@@ -0,0 +1,20 @@
+
+
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int X = scanner.nextInt();
+
+        System.out.println(X*24);
+
+//지문이 일본어 인데 감으로 풀어보아따
+
+    }
+
+}
