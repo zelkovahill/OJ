e29993aafbf0ac07191c5028897b740240d39893
ChangWooMin <changwoomin426@gmail.com>
2023-08-06 16:19:40 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 204 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,23 @@
+
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String []args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        int d=scanner.nextInt();
+
+        int A = (a+b+c+d)/60;
+        int B =(a+b+c+d)%60;
+        
+        System.out.println(A);
+        System.out.println(B);
+
+    }
+}
