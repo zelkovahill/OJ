7d28e5eca6b9c380b0687a74ccc11f09585da21d
ChangWooMin <changwoomin426@gmail.com>
2023-08-07 15:18:43 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 64 ms, Memory: 5780 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cs
@@ -0,0 +1,18 @@
+
+    String[] A = Console.ReadLine().Split();
+
+    int N = int.Parse(A[0]);
+    int K = int.Parse(A[1]);
+
+for(int i=0;i<N;i++)
+{
+    String[] B = Console.ReadLine().Split();
+
+    int C = int.Parse(B[0]);
+    int D = int.Parse(B[1]);
+}
+
+    
+
+
+Console.WriteLine("비와이");
