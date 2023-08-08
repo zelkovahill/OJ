0c1fac706cabf4f372a0d8594c6a44f64fa1e774
ChangWooMin <changwoomin426@gmail.com>
2023-08-08 19:13:42 +0900

    [Bronze V] Title: Plane, Time: 64 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cs
@@ -0,0 +1,8 @@
+string[] a = Console.ReadLine()!.Split();
+
+int n1 = int.Parse(a[0]);
+int n2 = int.Parse(a[1]);
+int k1 = int.Parse(a[2]);
+int k2 = int.Parse(a[3]);
+
+Console.WriteLine(n1 * n2 + k1 * k2);
