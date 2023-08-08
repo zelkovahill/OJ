a26c13bf1b4c151dc27e1f79bf30a5a9b9b1e62a
ChangWooMin <changwoomin426@gmail.com>
2023-08-08 22:41:40 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 220 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,20 @@
+
+
+
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String []args)
+    {
+        Scanner scanner =new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        a-=1;
+
+        System.out.println(a/5+1);
+
+
+    }
+}
