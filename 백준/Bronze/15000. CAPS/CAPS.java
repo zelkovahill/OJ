a3a8bb8a7288c306b46fdb6cf4b77683352f1559
ChangWooMin <changwoomin426@gmail.com>
2023-08-08 22:38:04 +0900

    [Bronze V] Title: CAPS, Time: 568 ms, Memory: 31732 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,20 @@
+
+
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String []args)
+    {
+        Scanner scanner =new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String output = input.toUpperCase();
+        System.out.println(output);
+
+
+
+    }
+}
