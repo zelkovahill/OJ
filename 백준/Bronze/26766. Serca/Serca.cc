24913b2b72c24cef36324e01f917a354d2ea0d81
ChangWooMin <changwoomin426@gmail.com>
2023-08-15 17:29:38 +0900

    [Bronze V] Title: Serca, Time: 160 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+int main()
+{
+
+
+    cin>>N;
+    while(N--)
+    {
+        cout << " @@@   @@@ " << '\n';
+		cout << "@   @ @   @" << '\n';
+		cout << "@    @    @" << '\n';
+		cout << "@         @" << '\n';
+		cout << " @       @ " << '\n';
+		cout << "  @     @  " << '\n';
+		cout << "   @   @   " << '\n';
+		cout << "    @ @    " << '\n';
+		cout << "     @     " << '\n';
+    }
+}
