19ea996c7014ff68a55dd878da1c1af13dec7de1
ChangWooMin <changwoomin426@gmail.com>
2023-08-15 16:41:10 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 896 ms, Memory: 6564 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cs
@@ -0,0 +1,30 @@
+int N = int.Parse(Console.ReadLine());
+
+for(int i =1;i<=N;i++)
+{
+    for(int j=1;j<=N-i;j++)
+    {
+        Console.Write(" ");
+    }
+
+    for(int j=1;j<=i;j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+for(int i=N-1;i>0;i--)
+{
+    for(int j =1;j<=N-i;j++)
+    {
+        Console.Write(" ");
+    }
+
+    for(int j=1;j<=i;j++)
+    {
+        Console.Write("*");
+    }
+
+    Console.WriteLine();
+}
