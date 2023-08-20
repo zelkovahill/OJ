668adba8d175cb3384ba33e7bae7868fc6fe773b
ChangWooMin <changwoomin426@gmail.com>
2023-08-20 14:59:05 +0900

    [Bronze V] Title: Site Score, Time: 68 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cs
@@ -0,0 +1,9 @@
+string[] A = Console.ReadLine().Split();
+
+int a = int.Parse(A[0]);
+int b = int.Parse(A[1]);
+int c = int.Parse(A[2]);
+int d = int.Parse(A[3]);
+
+int sum = 56 * a + 24 * b + 14 * c + 6 * d;
+Console.WriteLine(sum);
