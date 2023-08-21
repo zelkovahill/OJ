2363795941b9782fe4fb89152078ad4caba84cb8
ChangWooMin <changwoomin426@gmail.com>
2023-08-21 16:17:09 +0900

    [Bronze V] Title: 三方比較 (Three-Way Comparison), Time: 60 ms, Memory: 5456 KB -BaekjoonHub

---
+++ 백준/Bronze/27328. 三方比較 （Three－Way Comparison）/三方比較 （Three－Way Comparison）.cs
@@ -0,0 +1,15 @@
+var A = int.Parse(Console.ReadLine()!);
+var B = int.Parse(Console.ReadLine()!);
+
+
+if (A<B)
+{
+    Console.WriteLine("-1");
+}
+
+else if(A==B) Console.WriteLine("0");
+
+else
+{
+    Console.WriteLine("1");
+}
