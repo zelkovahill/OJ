1ec684ba20bbed8e0ec2cdf191e8aaf5fbba8ef2
ChangWooMin <changwoomin426@gmail.com>
2023-08-21 16:32:36 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 60 ms, Memory: 5360 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cs
@@ -0,0 +1,15 @@
+var Input = Console.ReadLine();
+
+switch(Input)
+{
+
+    case "SONGDO": Console.WriteLine("HIGHSCHOOL"); break;
+    case "CODE": Console.WriteLine("MASTER"); break;
+    case "2023": Console.WriteLine("0611"); break;
+    case "ALGORITHM": Console.WriteLine("CONTEST"); break;
+
+
+
+
+
+}
