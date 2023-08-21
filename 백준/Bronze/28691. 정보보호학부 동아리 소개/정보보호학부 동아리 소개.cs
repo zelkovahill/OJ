4562afec61748a5e2a0179e5cbb0b9865aaa193e
ChangWooMin <changwoomin426@gmail.com>
2023-08-21 16:38:39 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 60 ms, Memory: 5364 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cs
@@ -0,0 +1,10 @@
+var Input = Console.ReadLine()!;
+
+switch(Input)
+{
+    case "M": Console.WriteLine("MatKor"); break;
+    case "W": Console.WriteLine("WiCys"); break;
+    case "C": Console.WriteLine("CyKor"); break;
+    case "A": Console.WriteLine("AlKor"); break;
+    case "$": Console.WriteLine("$clear"); break;
+}
