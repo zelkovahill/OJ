6cadee15aaa9e9e4b60931553ed016db31c35471
ChangWooMin <changwoomin426@gmail.com>
2023-08-21 14:42:06 +0900

    [Bronze V] Title: Mathematics, Time: 64 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.cs
@@ -0,0 +1,17 @@
+
+
+int N = int.Parse(Console.ReadLine()!);
+
+
+
+int sum = 0;
+
+for(int i =0; i<N;i++)
+{
+    var A = int.Parse(Console.ReadLine()!);
+    
+    sum += A;
+
+}
+
+Console.WriteLine(sum);
