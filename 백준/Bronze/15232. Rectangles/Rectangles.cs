91d4f9f347086af09b4aa738d6ba0c69dc1d5d3c
ChangWooMin <changwoomin426@gmail.com>
2023-08-28 12:26:53 +0900

    [Bronze IV] Title: Rectangles, Time: 80 ms, Memory: 5548 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.cs
@@ -0,0 +1,11 @@
+var n1 = int.Parse(Console.ReadLine()!);
+var n2 = int.Parse(Console.ReadLine()!);
+
+for(int i=0;i<n1;i++)
+{
+    for(int j=0;j<n2;j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
