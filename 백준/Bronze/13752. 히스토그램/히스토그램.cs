49d34b54d159b0ecea7f511e9f5ef6599a4b7fdb
ChangWooMin <changwoomin426@gmail.com>
2023-08-29 14:46:02 +0900

    [Bronze IV] Title: 히스토그램, Time: 512 ms, Memory: 6616 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cs
@@ -0,0 +1,16 @@
+var T = int.Parse(Console.ReadLine()!);
+
+while (T > 0)
+{
+    var N = int.Parse(Console.ReadLine()!);
+
+    for (int i = 0; i < N; i++)
+    {
+        Console.Write("=");
+        
+    }
+
+    Console.WriteLine();
+
+    T--;
+}
