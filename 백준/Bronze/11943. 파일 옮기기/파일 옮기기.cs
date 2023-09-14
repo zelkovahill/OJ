22203974b654ab539dc67d55e574f033d8c9a869
ChangWooMin <changwoomin426@gmail.com>
2023-09-14 19:51:56 +0900

    [Bronze IV] Title: 파일 옮기기, Time: 60 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/11943. 파일 옮기기/파일 옮기기.cs
@@ -0,0 +1,21 @@
+string[] a =Console.ReadLine()!.Split(' ');
+
+int A = int.Parse(a[0]);
+int B = int.Parse(a[1]);
+
+string[] b =Console.ReadLine()!.Split(' ');
+
+int C = int.Parse(b[0]);
+int D = int.Parse(b[1]);
+
+
+
+if (A+D < B+C) Console.WriteLine(A + D);
+else Console.WriteLine(B + C);
+// 문제가 이상함
+
+
+
+
+
+
