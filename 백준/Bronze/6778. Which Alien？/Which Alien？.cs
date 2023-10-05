be0111e0f446db0e1ddca7acc52d9ac346da9170
Chang WooMin <changwoomin426@gmail.com>
2023-10-05 18:59:30 +0900

    [Bronze IV] Title: Which Alien?, Time: 60 ms, Memory: 5456 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.cs
@@ -0,0 +1,6 @@
+var n1 = int.Parse(Console.ReadLine());
+var n2 = int .Parse(Console.ReadLine());
+
+if (3 <= n1 && n2 <= 4) Console.WriteLine("TroyMartian");
+if (6 >= n1 && n2 >= 2) Console.WriteLine("VladSaturnian");
+if (2 >= n1 && n2 <= 3) Console.WriteLine("GraemeMercurian");
