9229071ddab80f3a3656e0ab8acfaf2ca5f7c359
Chang WooMin <changwoomin426@gmail.com>
2023-10-06 13:59:08 +0900

    [Bronze IV] Title: ISBN, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cout.tie(0);
+    cin.tie(0);
+int a,b,c;
+cin>>a>>b>>c;
+cout << "The 1-3-sum is "<<91+a*1+b*3+c*1<<"\n";
+
+}
