e4a911dafd95e30f18791027b05d7e73908144a2
Chang WooMin <changwoomin426@gmail.com>
2023-12-16 13:10:49 +0900

    [Bronze IV] Title: Counting Clauses, Time: 60 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/17903. Counting Clauses/Counting Clauses.cs
@@ -0,0 +1,8 @@
+string [] i =Console.ReadLine().Split(' ');
+int m = int.Parse(i[0]),
+       n = int.Parse(i[1]);
+       
+   
+           if(m>=8) Console.WriteLine("satisfactory");
+           else Console.WriteLine("unsatisfactory");
+           
