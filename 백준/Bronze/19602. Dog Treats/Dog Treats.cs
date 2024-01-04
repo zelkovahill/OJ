06a67874181e716e5a1d0cf7975db624e98b81fc
Chang WooMin <changwoomin426@gmail.com>
2024-01-04 20:46:34 +0900

    [Bronze IV] Title: Dog Treats, Time: 60 ms, Memory: 5460 KB -BaekjoonHub

---
+++ 백준/Bronze/19602. Dog Treats/Dog Treats.cs
@@ -0,0 +1,8 @@
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+
+int res = a * 1 + b * 2 + c * 3;
+
+
+Console.WriteLine(res >= 10 ?"happy":"sad");
