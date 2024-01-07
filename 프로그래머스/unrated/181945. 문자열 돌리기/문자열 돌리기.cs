a009ba0c5fbd5398d5b89aea876a5e07a257a1bf
Chang WooMin <changwoomin426@gmail.com>
2024-01-07 16:56:12 +0900

    [level unrated] Title: 문자열 돌리기, Time: 17.50 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+       
+
+        
+        string str = Console.ReadLine();
+        
+        foreach(var Str in str)
+        {
+            Console.WriteLine("{0}", Str);
+        }
+
+    }
+}
